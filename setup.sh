4e4557dd8f9dcb96e166574175bfce591aa6ba78
jiaqi <xuejiaqi001@gmail.com>
2025-07-17 15:25:03 -0400

    update: readme

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@ mkdir seallibs
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX=./seallibs
 cmake --build build
 sudo cmake --install build
+
+git clone https://github.com/google/benchmark.git
+cd benchmark
+git clone https://github.com/google/googletest.git
+cmake -E make_directory build
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/jiaq/Research/SEAL/benchmarklibs -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON
+cmake --build build --config Release
+cmake --install build --config Release
